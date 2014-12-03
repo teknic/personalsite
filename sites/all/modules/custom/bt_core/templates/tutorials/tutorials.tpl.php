b43b740046d2c82244d3f360663272e91db56094
teknic <btate340@gmail.com>
2014-12-03 15:54:00 -0800

    added code filter for code tags

--- sites/all/modules/custom/bt_core/templates/tutorials/tutorials.tpl.php
+++ sites/all/modules/custom/bt_core/templates/tutorials/tutorials.tpl.php
@@ -1,15 +1,15 @@
-<div class="blogs grid row">
-  <?php foreach($blogs as $blog): ?>
-    <div class="blog col-lg-4 col-sm-6">
+<div class="tutorials grid row">
+  <?php foreach($tutorials as $tutorial): ?>
+    <div class="tutorial col-lg-4 col-sm-6">
       <div class="grid-box">
-        <a class="grid-img-holder" href="/<?php echo $blog['url']; ?>" style="background-image:url(<?php echo $blog['image_style']; ?>)"></a>
+        <a class="grid-img-holder" href="/<?php echo $tutorial['url']; ?>" style="background-image:url(<?php echo $tutorial['image_style']; ?>)"></a>
         <div class="grid-content">
-          <div class="date grid-time"><?php echo $blog['display_date']; ?></div>
-          <div class="title"><h3><a href="/<?php echo $blog['url']; ?>"><?php echo $blog['title']; ?></a></h3></div>
+          <div class="date grid-time"><?php echo $tutorial['display_date']; ?></div>
+          <div class="title"><h3><a href="/<?php echo $tutorial['url']; ?>"><?php echo $tutorial['title']; ?></a></h3></div>
         </div>
         <div class="grid-bottom">
           <div class="grid-tags">
-            <?php foreach($blog['tags'] as $tag): ?>
+            <?php foreach($tutorial['tags'] as $tag): ?>
               <a href="/<?php echo $tag['url']; ?>"><?php echo $tag['name']; ?></a>
             <?php endforeach; ?>
           </div>
