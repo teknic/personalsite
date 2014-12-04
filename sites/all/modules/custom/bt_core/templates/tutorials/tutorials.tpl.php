2a430f908de9e9527abfb4bc6137cfd021b482e3
teknic <btate340@gmail.com>
2014-12-03 20:32:30 -0800

    added contact us, resume, and updated tutorials

--- sites/all/modules/custom/bt_core/templates/tutorials/tutorials.tpl.php
+++ sites/all/modules/custom/bt_core/templates/tutorials/tutorials.tpl.php
@@ -1,18 +1,12 @@
-<div class="tutorials grid row">
+<div class="tutorials row">
   <?php foreach($tutorials as $tutorial): ?>
-    <div class="tutorial col-lg-4 col-sm-6">
-      <div class="grid-box">
-        <a class="grid-img-holder" href="/<?php echo $tutorial['url']; ?>" style="background-image:url(<?php echo $tutorial['image_style']; ?>)"></a>
-        <div class="grid-content">
-          <div class="date grid-time"><?php echo $tutorial['display_date']; ?></div>
-          <div class="title"><h3><a href="/<?php echo $tutorial['url']; ?>"><?php echo $tutorial['title']; ?></a></h3></div>
-        </div>
-        <div class="grid-bottom">
-          <div class="grid-tags">
-            <?php foreach($tutorial['tags'] as $tag): ?>
-              <a href="/<?php echo $tag['url']; ?>"><?php echo $tag['name']; ?></a>
-            <?php endforeach; ?>
-          </div>
+    <div class="tutorial-wrapper col-lg-4 col-md-4 col-sm-6 col-xs-12">
+      <div class="tutorial-box" style="background-image:url(<?php echo $tutorial['image_style']; ?>)">
+        <div class="overlay">
+          <a href="/<?php echo $tutorial['url']; ?>" class="title">
+            <span><?php echo $tutorial['title']; ?></span>
+          </a>
+          <span><?php echo $tutorial['difficulty']; ?></span>
         </div>
       </div>
     </div>
