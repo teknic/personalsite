6b400304b4bd459f813282ba51f2b98c2bf4a0de
teknic <btate340@gmail.com>
2014-12-07 11:36:09 -0800

    added calendar icon to blog listing

--- sites/all/modules/custom/bt_core/templates/blog/blog-list.tpl.php
+++ sites/all/modules/custom/bt_core/templates/blog/blog-list.tpl.php
@@ -4,7 +4,7 @@
       <div class="grid-box">
         <a class="grid-img-holder" href="/<?php echo $blog['url']; ?>" style="background-image:url(<?php echo $blog['image_style']; ?>)"></a>
         <div class="grid-content">
-          <div class="date grid-time"><?php echo $blog['display_date']; ?></div>
+          <div class="date grid-time"><i class="fa fa-calendar-o"></i><?php echo $blog['display_date']; ?></div>
           <div class="title"><h3><a href="/<?php echo $blog['url']; ?>"><?php echo $blog['title']; ?></a></h3></div>
         </div>
         <div class="grid-bottom">
